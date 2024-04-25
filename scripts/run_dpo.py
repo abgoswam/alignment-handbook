a11729212bb645562ad868ed5e0c604655d6e12d
Abhishek Goswami <agoswami@microsoft.com>
2024-04-25 11:16:18 -0700

    able to run DPO for both mixtral8x7 and phi_moe_hf

--- scripts/run_dpo.py
+++ scripts/run_dpo.py
@@ -17,9 +17,10 @@ import logging
 import random
 import sys
 
-from sympy import false
 import torch
 import transformers
+import time
+import deepspeed
 from transformers import AutoModelForCausalLM, set_seed
 
 from alignment import (
@@ -46,8 +47,6 @@ logger = logging.getLogger(__name__)
 
 
 def main():
-
-    is_change_from_canxu = False
     parser = H4ArgumentParser((ModelArguments, DataArguments, DPOConfig))
     model_args, data_args, training_args = parser.parse()
 
@@ -152,86 +151,66 @@ def main():
         quantization_config=quantization_config,
     )
 
-    if is_change_from_canxu:
-        print("Following model loading changes from Can...")
+    # hack to get past permission issues in AML when multiple processes attempt copying HF files 
+    # https://github.com/microsoft/genai/issues/469
+    time.sleep(random.uniform(1, 10))  
+    for i in range(32,0,-1):
+        try:
+            model = AutoModelForCausalLM.from_pretrained(model_args.model_name_or_path, **model_kwargs)
+            break
+        except PermissionError as e:
+            if i > 1:
+                print(f"Model encounters PermissionError: {e}")
+                time.sleep(10)
+                continue
+            else:
+                raise e
+    logger.info("*** Model: loaded! ***")
+    
+    for m in model.modules():
+        if "MixtralSparseMoeBlock" in m.__class__.__name__:
+            target_cls = m.__class__
+    deepspeed.utils.set_z3_leaf_modules(model, [target_cls])
+    logger.info("*** Model: zero3 leaf set successfully! ***")
 
-        model = transformers.AutoModelForCausalLM.from_pretrained(
-            model_args.model_name_or_path,
-            use_flash_attention_2 = True,
-            torch_dtype=torch.bfloat16,
-        )
-        model.config.use_cache = False
-        
-        # print("Load Refer Model")
-        model_refer = transformers.AutoModelForCausalLM.from_pretrained(
-            model_args.model_name_or_path,
-            use_flash_attention_2 = True,
-            torch_dtype=torch.bfloat16,
-        )
+    # hack to get past permission issues in AML when multiple processes attempt copying HF files 
+    # https://github.com/microsoft/genai/issues/469
+    time.sleep(random.uniform(1, 10))  
+    for i in range(32,0,-1):
+        try:
+            ref_model = transformers.AutoModelForCausalLM.from_pretrained(model_args.model_name_or_path, **model_kwargs)
+            break
+        except PermissionError as e:
+            if i > 1:
+                print(f"Ref Model encounters PermissionError: {e}")
+                time.sleep(10)
+                continue
+            else:
+                raise e
+    logger.info("*** Ref Model: loaded! ***")
 
-        trainer = DPOTrainer(
-            model, 
-            model_refer, 
-            tokenizer = tokenizer, 
-            beta = training_args.beta, 
-            args=training_args, 
-            train_dataset = raw_datasets["train"],
-            max_prompt_length = 512, 
-            max_length = training_args.max_length,
-        )
+    for m in ref_model.modules():
+        if "MixtralSparseMoeBlock" in m.__class__.__name__:
+            target_cls = m.__class__
+    deepspeed.utils.set_z3_leaf_modules(ref_model, [target_cls])
+    logger.info("*** Ref Model: zero3 leaf set successfully! ***")
 
-    else:
-        print("Following original model loading...")
-
-        model = model_args.model_name_or_path
-        if is_adapter_model(model, model_args.model_revision) is True:
-            logger.info(f"Loading SFT adapter for {model_args.model_name_or_path=}")
-            peft_config = PeftConfig.from_pretrained(model_args.model_name_or_path, revision=model_args.model_revision)
-            model_kwargs = dict(
-                revision=model_args.base_model_revision,
-                trust_remote_code=model_args.trust_remote_code,
-                use_flash_attention_2=model_args.use_flash_attention_2,
-                torch_dtype=torch_dtype,
-                use_cache=False if training_args.gradient_checkpointing else True,
-                device_map=get_kbit_device_map() if quantization_config is not None else None,
-                quantization_config=quantization_config,
-            )
-            base_model = AutoModelForCausalLM.from_pretrained(
-                peft_config.base_model_name_or_path,
-                **model_kwargs,
-            )
-            model = PeftModel.from_pretrained(
-                base_model,
-                model_args.model_name_or_path,
-                revision=model_args.model_revision,
-            )
-            model_kwargs = None
-
-        ref_model = model
-        ref_model_kwargs = model_kwargs
-
-        if model_args.use_peft is True:
-            ref_model = None
-            ref_model_kwargs = None
-
-        #########################
-        # Instantiate DPO trainer
-        #########################
-        trainer = DPOTrainer(
-            model,
-            ref_model,
-            model_init_kwargs=model_kwargs,
-            ref_model_init_kwargs=ref_model_kwargs,
-            args=training_args,
-            beta=training_args.beta,
-            train_dataset=raw_datasets["train"],
-            eval_dataset=raw_datasets["test"],
-            tokenizer=tokenizer,
-            max_length=training_args.max_length,
-            max_prompt_length=training_args.max_prompt_length,
-            peft_config=get_peft_config(model_args),
-            loss_type=training_args.loss_type,
-        )
+    #########################
+    # Instantiate DPO trainer
+    #########################
+    trainer = DPOTrainer(
+        model,
+        ref_model,
+        args=training_args,
+        beta=training_args.beta,
+        train_dataset=raw_datasets["train"],
+        eval_dataset=raw_datasets["test"],
+        tokenizer=tokenizer,
+        max_length=training_args.max_length,
+        max_prompt_length=training_args.max_prompt_length,
+        peft_config=get_peft_config(model_args),
+        loss_type=training_args.loss_type,
+    )
 
     ###############
     # Training loop
